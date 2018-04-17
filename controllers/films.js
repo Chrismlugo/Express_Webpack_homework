147e7b4f1cdfe22c04b0e9a752a22382da8f7b92
Chris Murphy <_chrism_@live.com>
2018-04-17 09:20:28 +0100

    fixes bug. could not require films into films.js controller. adds genre to films

--- controllers/films.js
+++ controllers/films.js
@@ -1,33 +1,43 @@
 //since we don't have a database we'll use our front end models at the moment
 var express = require('express');
 var filmsRouter = new express.Router();
-var films = require('../client/src/models/films');
+var films = require('../client/src/models/films')();
 var Film = require('../client/src/models/film');
 var Review = require('../client/src/models/review');
 
-var filmsArray = ["Indiana Jones and the last crusade", "Inception", "Catch me If you can"]
+// var film1 = new Film({
+//   title: "The Big Lebowski",
+//   actors: ["Jeff Briges", "John Goodman"]
+// })
+//
+// var film2 = new Film({
+//   title: "Fight Club",
+//   actors: ["Edward Norton", "Meatloaf"]
+// })
+
+// var filmsArray = ["Indiana Jones and the last crusade", "Inception", "Catch me If you can"]
 
 filmsRouter.get('/:id', function(req,res){
-  res.json({data: filmsArray[req.params.id]});
+  res.send({data: films[req.params.id]});
 })
 
 filmsRouter.get('/', function(req,res){
-  res.json({data: filmsArray});
+  res.json({data: films});
 })
 
 filmsRouter.post('/', function(req,res){
-  filmsArray.push(req.body.film);
-  res.json({data: filmsArray});
+  films.push(req.body.film);
+  res.json({data: films});
 })
 
 filmsRouter.put('/:id', function(req,res){
-  filmsArray[req.params.id] = req.body.film;
-  res.json({data: filmsArray});
+  films[req.params.id] = req.body.film;
+  res.json({data: films});
 })
 
 filmsRouter.delete('/:id', function(req,res){
   filmsArray.splice(req.params.id, 1);
-  res.json({data: filmsArray});
+  res.json({data: films});
 })
 
 module.exports = filmsRouter;
